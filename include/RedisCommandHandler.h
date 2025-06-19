3f93b8dcaf5dad286ed80b922388786a40a2c5ad
Ajay <negia2582@gmail.com>
2025-06-19 23:35:06 +0530

    Add Redis Command handle support

--- include/RedisCommandHandler.h
+++ include/RedisCommandHandler.h
@@ -2,7 +2,15 @@
 // Created by ajay on 18/6/25.
 //
 
+ #include <string>
+
 #ifndef REDISCOMMANDHANDLER_H
-#define REDISCOMMANDHANDLER_H
+#define REDISCOMMANDHANDLER_H\
+
+class RedisCommandHandler {
+public:
+    RedisCommandHandler();
+    std::string processCommand(const std::string &command);
+};
 
 #endif //REDISCOMMANDHANDLER_H
